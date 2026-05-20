b718bec0c54b4fc0b599cab2f924b5f16bc97e7d
hafiz11339 <hafizumair.ie@gmail.com>
2026-05-20 16:39:32 +0500

    udpate the varible file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
-variable "umair" {
-  description = "Your GitHub username (lowercase)"
+variable "aws_region" {
+  default = "us-east-1"
 }
 
 variable "app_name" {
   default = "fastapi-app"
+}
+
+variable "github_username" {
+  default = "hafiz11339"    ← your GitHub username
 }
