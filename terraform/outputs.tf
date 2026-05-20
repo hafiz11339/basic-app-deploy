4c18b52e24ccb9ba5e8865a6d21570336d9ebdfa
hafiz11339 <hafizumair.ie@gmail.com>
2026-05-20 19:48:35 +0500

    update output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,7 @@ output "docs_url" {
   value       = "http://${aws_instance.app.public_ip}:8000/docs"
   description = "FastAPI auto-generated docs"
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.app.public_ip
+}
